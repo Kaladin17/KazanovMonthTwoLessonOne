df6c23e81e912bfd43c1075d6d5794b4540b43a2
Kaladin17 <dekazanov8@gmail.com>
2023-03-11 14:56:30 +0600

    Initial commit

---
+++ app/src/main/java/com/kazanov/kazanovmonthtwolessonone/MainActivity.java
@@ -0,0 +1,42 @@
+package com.kazanov.kazanovmonthtwolessonone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton button;
+    EditText one;
+    EditText two;
+    EditText three;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        one = findViewById(R.id.first_name);
+        two = findViewById(R.id.last_name);
+        three = findViewById(R.id.age);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            if (!one.getText().toString().isEmpty() & !two.getText().toString().isEmpty() & !three.getText().toString().isEmpty()){
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("key1", one.getText().toString());
+                intent.putExtra("key2", two.getText().toString());
+                intent.putExtra("key3", three.getText().toString());
+                startActivity(intent);
+            }else {
+                Toast.makeText(MainActivity.this,"Enter all fields", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
